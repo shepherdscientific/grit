74fadb8bf92aa42303ba4a4ee0d6785e44d5945a
yoda <yoda@grit.systems>
2022-05-01 14:52:24 +0100

    grit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-// import './App.css';
+import './App.css';
 import Model from './Grit'
 import { Canvas, useFrame } from '@react-three/fiber'
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Model position={[0, -12, 0]} rotation={[1.6,0,0]} scale={[10,10,10]}  />   
+      <Model position={[0, -0.7, 0]} rotation={[4 * Math.PI / 7 , 0 , 0]} scale={[ 1, 1 , 1]}  />   
       {/* <BoxCube position={[2.465, 0.85, -1.47]} /> */}
       
     </Canvas>
-    </div>
+    // </div>
   );
 }
 
