90deda3b12cd89ecbfe4756a46ff7ae821fa5288
yoda <yoda@grit.systems>
2022-05-01 19:04:41 +0100

    orbitcontrols

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Model from './Grit'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import React, { useRef, useState } from 'react'
+import { OrbitControls, Stars, useCursor, MeshReflectorMaterial, Image, Text, Environment } from '@react-three/drei'
 
 function Grit(props){
   return (
     <>
-    <Model position={[0, -0.7, 0]} rotation={[4 * Math.PI / 7 , 0 , 0]} scale={[ 1, 1 , 1]}  />   
+    <Model position={[0, -0.5, 0]} rotation={[4 * Math.PI / 7 , 0 , 0]} scale={[ 1, 1 , 1]}  />   
     <BoxCube position={[2.465, 0.85, -1.47]} />
     </>
   );
@@ -21,23 +22,25 @@ function Box(props) {
     <mesh
       {...props}
       ref={mesh}
-      scale={active ? 1.5 : 1}
+      scale={active ? 1.1 : 1}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}>
       <boxGeometry args={[1.25, 1.25, 1.25]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'green'} />
+      <meshStandardMaterial color={hovered ? 'yellow' : 'green'} />
     </mesh>
   );
 }
 
 function BoxCube(props){
+  // Calculate positions of eight or eighteen cubes
   return (
     <>
     <Box position={props.position} />
+    <Box />
+    {/* <Box position={props.position} />
     <Box position={props.position} />
-    <Box position={props.position} />
-    <Box position={props.position} />
+    <Box position={props.position} /> */}
     </>
   );
 }
@@ -47,10 +50,30 @@ function App() {
   return (
     // <div className="App">
     <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Grit />
-      
+      <OrbitControls/>
+      <Stars/>
+      <color attach="background" args={['#191920']} />
+      <fog attach="fog" args={['#191920', 0, 15]} />
+      <Environment preset="city" />   
+      <group position={[0, -0.2, 0]}>
+        <pointLight position={[10, 10, 10]} />
+        <Grit position={[0, -10, 0]} />
+        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+          <planeGeometry args={[50, 50]} />
+          <MeshReflectorMaterial
+            blur={[300, 100]}
+            resolution={2048}
+            mixBlur={1}
+            mixStrength={40}
+            roughness={1}
+            depthScale={1.2}
+            minDepthThreshold={0.4}
+            maxDepthThreshold={1.4}
+            color="#101010"
+            metalness={0.5}
+          />
+        </mesh>
+      </group>               
     </Canvas>
     // </div>
   );
